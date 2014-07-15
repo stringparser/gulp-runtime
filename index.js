ff8d139df1c7bde6e2fb616488ef0dbcf9f76c08
Javier Carrillo <stringparser@gmail.com>
2014-07-15 23:25:10 +0200

    yeap

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var gupl = require('gulp');
 
-gulp.task('default', function(){
-  console.log('hello '+process.argv);
-})
 
-process.stdin.resume();
-process.stdin.on('data', function(chunk){
-  console.log(chunk)
-})
