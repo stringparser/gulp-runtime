dbb81868f6d74f0157f466074ba5a5ac72e775ff
Javier Carrillo <stringparser@gmail.com>
2014-07-21 13:09:29 +0200

    working more or less

--- utils/childGulp.js
+++ utils/childGulp.js
@@ -11,19 +11,6 @@ module.exports = function childGulp(argv){
   if(argv[0] !== '--color' && argv[0] !== '--no-color')
     argv.unshift('--color');
 
-  stdout.write('\n Child gulp started '+ JSON.stringify(argv) + '\n\n');
-  var child = spawn('gulp', argv);
-
-  // Don't keep parent waiting
-  child.stdin.end();
-
-  // Handle output
-  child.stdout.on('data', function(chunk){
-    stdout.write(chunk)
-  }).on('end', function(){
-    stdout.write(promptText);
-    child.kill();
-  })
-
-  return false;
+  /*stdout.write('\n Child gulp started '+ JSON.stringify(argv) + '\n\n');*/
+  return spawn('gulp', argv);
 }
