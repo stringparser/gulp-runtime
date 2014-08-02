67a75395dbf26e785565a8662c78a402d21c2d86
Javier Carrillo <stringparser@gmail.com>
2014-08-02 13:37:48 +0200

    refactor prior to 0.2.0

--- test/index.js
+++ test/index.js
@@ -2,23 +2,14 @@
 var prettyfy = require('../util/prettyfy');
 var runtime = require('../lib/runtime')('gulp');
 
-
 runtime.onStartup(function(){
-/*  console.log('\n Runtime handlers \n')
-  console.log(this._Handle)
-  console.log('\n  Startup runtime: \n')
-  console.log(this)*/
-})
-
-runtime.onStartup(function(){
-  console.log('\n Command proposal', this.command('hello'))
-  console.log('')
   this.prompt();
 })
 
-runtime.set('this', function(){
-  console.log(this);
-  console.log(arguments)
-}).command('that', function(){
-  console.log('that')
+runtime.set('first', function First(){
+  console.log('first!')
+}).set('second', function Second(){
+  console.log('hello')
 })
+
+
