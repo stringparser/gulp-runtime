3dd2536a4139b6d1dfc36d6854b53af7f40c3933
Javier Carrillo <stringparser@gmail.com>
2014-08-12 12:33:57 +0200

    0.2.x -> updated to runtime@^0.1.18

--- test/index.js
+++ test/index.js
@@ -1,16 +1,2 @@
 
-var env = process.env;
-    env.NODE_ENV = env.NODE_ENV || 'dev-test';
 
-var runtime = new require('runtime').createInterface('terminal2');
-
-runtime.set('first', function(){
-  return 'first!'
-}).set('second', function(){
-  return 'second!'
-}).set('--some-flag', function(){
-  return 'whatever'
-})
-
-
-process.stdin.end();
