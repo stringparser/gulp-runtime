009d62f83bf17a9cf9a2089b6785b65232f23bef
Javier Carrillo <stringparser@gmail.com>
2014-09-17 19:11:37 +0200

    batch login

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var runtime = require('./lib/gulp-runtime');
 
+console.log(process.execArgv);
+console.log(process.argv);
+console.log(runtime.config('env'));
+
 gulp.task('develop', function () {
 
 });
@@ -10,10 +14,22 @@ gulp.task('lint', function(){
 });
 
 gulp.task('another', function(){
-  
-})
+
+});
+
+gulp.task('anotherOne', function(){
+
+});
+
+gulp.task('anotherMore', function(){
+
+});
+
+gulp.task('andAnotherOneMore', function(){
+
+});
 
 // build everything and open main entry page
-gulp.task('default', ['develop'], function () {
+gulp.task('default', ['develop', 'lint', 'another', 'anotherOne', 'anotherMore', 'andAnotherOneMore'], function () {
     // ...
 });
