6fad4a96deb451e18b049332041e092fc011194d
Javier Carrillo <stringparser@gmail.com>
2014-10-09 17:52:24 +0200

    runtime update, mayor update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
-var runtime = require('./lib/gulp-runtime');
+var runtime = require('./.');
 
 console.log(process.execArgv);
 console.log(process.argv);
-console.log(runtime.config('env'));
 
 var testTasks = ['lint', 'jade', 'stylus', 'js', 'jsx'];
 testTasks.forEach(function(name){
@@ -17,9 +16,17 @@ testTasks.forEach(function(name){
 var browdeps = ['js', 'jsx'];
 gulp.task('browserify', browdeps, function(){
 
-
+  console.log('updated!')
 });
 // build everything and open main entry page
 gulp.task('default', testTasks.slice(0, 3).concat('browserify'), function () {
     // ...
+});
+
+gulp.watch(__filename).on('change', function(){
+  delete require.cache[__filename];
+  require(__filename);
+  process.nextTick(function(){
+    gulp.start('default');
+  });
 });
