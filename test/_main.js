d782000b03fd66a9f1d13fbe57d94d5c20fe0d78
Javier Carrillo <stringparser@gmail.com>
2014-10-09 18:59:22 +0200

    have to fix those test

--- test/_main.js
+++ test/_main.js
@@ -1,18 +1,17 @@
 'use strict';
 
-var monkey = require('stdout-monkey');
+var monkey = require('stdout-monkey')();
 
 describe('gulp-runtime', function(){
 
   var fs = require('fs');
-  var stdout = hookStdout();
   var runtime = require('../.');
 
   var testFiles = fs.readdirSync('./test/suite');
 
   testFiles.forEach(function(testFile){
     describe(testFile.replace('.','/'), function(){
-      require('./suite/'+testFile)(runtime, stdout);
+      require('./suite/'+testFile)(runtime, monkey);
     });
 
   });
