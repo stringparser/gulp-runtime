6cd51419df09e778257acd4817f27e288ae1a09d
Javier Carrillo <stringparser@gmail.com>
2014-10-17 18:34:35 +0200

    starting tests again

--- test/_main.js
+++ test/_main.js
@@ -6,14 +6,14 @@ describe('gulp-runtime', function(){
 
   var fs = require('fs');
   var runtime = require('../.');
+      runtime.config('repl', false);
 
-  var testFiles = fs.readdirSync('./test/suite');
+/*  var testFiles = fs.readdirSync('./test/suite');
 
   testFiles.forEach(function(testFile){
     describe(testFile.replace('.','/'), function(){
       require('./suite/'+testFile)(runtime, monkey);
     });
-
-  });
+  });*/
 
 });
