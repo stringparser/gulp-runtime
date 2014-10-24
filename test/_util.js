4caa1eb6d4be564b9e3783b9873bc620a3f85e95
Javier Carrillo <stringparser@gmail.com>
2014-10-24 16:28:00 +0200

    new tests

---
+++ test/_util.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var fs = require('fs');
+
+module.exports = {
+  testSuite : function(){
+    var testSuite = fs.readdirSync(__dirname);
+    // in case there is priority
+    var testFirst = [];
+
+    // omit things starting with underscore
+    testSuite = testSuite.filter(function(thing){
+      return !thing.match(/^_/);
+    });
+    testSuite.unshift.apply(testSuite, testFirst);
+    return testSuite;
+  }
+};
