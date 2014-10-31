97945efe70cf1de5b2d99493ca540c414c7aa0a8
Javier Carrillo <stringparser@gmail.com>
2014-10-31 19:49:22 +0100

    refactor so input and output streams can be changed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-module.exports = require('./lib/gulp-runtime');
+'use strict';
+
+var runtime = require('runtime');
+var util = require('runtime/lib/utils');
+
+// <~> - <~>
+// make a repl by default
+// if not input and output will be through streams
+// <~> - <~>
+
+var instance;
+var argv = util.argv(process.argv);
+if( argv.repl === 'false' ){
+  instance = runtime.create('gulp');
+} else {
+  instance = runtime.create('gulp', {
+     input : process.stdin,
+    output : process.stdout
+  });
+}
+
+instance.require('./lib/utils');
+instance.require('./lib/init');
+instance.require('./lib/repl');
+
+module.exports = instance;
