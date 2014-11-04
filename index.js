77907983bc3bb4eb7f5156f7bdd688a584ef7502
Javier Carrillo <stringparser@gmail.com>
2014-11-04 12:59:16 +0100

    do not wait for startup to do checks, rigth from the begining

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var path = require('path');
-var util = require('runtime/lib/utils');
-
 // <~> - <~>
 // make a repl by default
 // if not input and output will be through streams
@@ -14,48 +11,15 @@ var runtime =  require('runtime')
     output : process.stdout
   });
 
-util.requirem('./lib/utils');
-
 //
-// ## do not wait for startup
-//    to do the checks
+// ## do not wait for startup to do the checks
+//
 
-var chalk = util.colors;
-var env = util.whech.sync('gulp');
-
-if( !env.globalPackage.version ){
-  runtime.output = process.stdout;
-  util.log(util.longBadge);
-  util.log(chalk.red('gulp is not installed globally'));
-  util.log('Try running: npm install gulp -g');
-  return process.exit(1);
-}
-
-if( !env.localPackage.version ){
-  runtime.output = process.stdout;
-  util.log(util.longBadge);
-  util.log(chalk.red('gulp is not installed locally'));
-  util.log('But is globally at', chalk.magenta(
-    util.tildify(env.globalDir) + 'gulp@' + env.globalPackage.version
-  ));
-  util.log('Try running: \'npm link gulp\' or \'npm install gulp\'');
-  return process.exit(1);
-}
-
-if( env.configFile instanceof Error ){
-  env.configFile = path.basename(env.configFile);
-  runtime.output = process.stdout;
-  util.log(util.longBadge);
-  util.log(chalk.red('No gulpfile ') + '\'' + env.configFile + '\'');
-  util.log(chalk.red(' found from ') + '\'' + env.cwd  + '\'');
-  return process.exit(1);
-}
+require('./lib/util/startupChecks')(runtime);
 
 //
 // ## save environment before init
 //
-
-runtime.config('env', util.merge(env, runtime.parser(argv)));
 runtime.require('./lib/init');
 runtime.require('./lib/repl');
 
