fc49876cbb34d0b5ed5651a55b7396d799985113
Javier Carrillo <stringparser@gmail.com>
2014-11-04 18:00:33 +0100

    logging and testing...

--- test/_main.js
+++ test/_main.js
@@ -4,7 +4,7 @@
 process.argv[1] = './test/_gulpfile.js';
 process.argv.splice(process.argv.indexOf('--repl'), 1);
 process.argv.push('--gulpfile', '_gulpfile.js');
-process.argv.push('--silent');
+process.argv.push('--silent', '--no-color');
 
 var path = require('path');
 var packageName = require('../package').name;
