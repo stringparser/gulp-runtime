1faca49d6002dd7015015fab5340fe1f5aa84791
Javier Carrillo <stringparser@gmail.com>
2015-03-25 23:02:14 +0100

    smoke tests, tomorrow the commands

--- example.js
+++ example.js
@@ -1,5 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
+var util = require('./lib/util');
+var runtime = require('./.').repl();
 
-gulp.task('name');
+runtime.task(':handle', function(next){
+  if(next.match === 'jsx'){
+    throw new Error('Parse Error: Unespected Identifier');
+  }
+ setTimeout(next, Math.random()*10);
+});
+
+runtime.task('default', ['css', 'jsx', 'img']);
+
+runtime.stack('default')();
