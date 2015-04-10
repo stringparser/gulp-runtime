9d8abf27e25610a1a664def8558d4674218d4f20
Javier Carrillo <stringparser@gmail.com>
2015-04-10 14:40:29 +0200

    smoke tests for the repl

--- example.js
+++ example.js
@@ -1,18 +1,12 @@
 'use strict';
 
-var gulp = require('gulp');
-var util = require('./lib/util');
-var app = require('./.').readline();
+var gulp = require('./.').repl();
 
-app.set(':handle(\\w+)', function(next){
+gulp.task(':handle(css|jsx|img)', function(next){
   if(next.match === 'jsx'){
     throw new Error('Parse Error: Unespected Identifier');
   }
  setTimeout(next, Math.random()*10);
 });
 
-var defaultHandle = app.stack('whatever',
-  app.stack('css jsx img', {wait: true}), {wait: true}
-);
-
-gulp.task('default', defaultHandle);
+gulp.stack('css jsx img', {wait: true})();
