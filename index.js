82ca8c966c6f29030da1e57d3b0142f565a42e0a
Javier Carrillo <stringparser@gmail.com>
2015-04-13 17:49:36 +0200

    ok watch is finished now tests

--- index.js
+++ index.js
@@ -27,64 +27,73 @@ tornado.Runtime.prototype.dest = vinylFS.dest;
 /*
 # runtime.watch
 ```js
-function watch(string|array globs[, array opt, function callback])
+function watch(string|array glob[, string|array|object opt, function cb])
 ```
-Same as [gulp.watch][m-gulp-watch]
-*/
-tornado.Runtime.prototype.watch = function(glob, opt, cb){
-  if(util.type(opt).match(/function|array/)){
-    cb = opt;
-    opt = null;
-  }
 
-  if(util.type(cb).array){
-    return vinylFS.watch(glob, opt,
-      this.stack(cb.join(' '))
+Same as [gulp.watch][m-gulp-watch] (run tasks after change with an optional
+ callback) with some aditional features:
+  - Files can be reloaded on the `require.cache`
+  - Tasks can be run in series directly if so specified
+
+_arguments_ differences with [gulp.watch][m-gulp-watch]
+ - `tasks` can be a string, space separated tasks will be dispatched as written
+ - when `opt` is an object, it can have non mandatory properties below
+  - `opt.wait` type boolean, if `opt.tasks` should run in series or not
+  - `opt.tasks` type array|string, tasks to run after change and before reload
+  - `opt.reload` type boolean, wether or not to reload a file
+
+_returns_
+ - a watcher, same as [gulp.watch][m-gulp-watch]
+
+> Notes:
+- The callback arguments are:
+  - a gaze event
+  - the `opt` argument given
+- Only files with a registered `require.extension` will be reloaded
+- When a file is deleted on disk, the reload will be skipped but the
+callback is still invoked
+
+```javascript
+var gulp = require('gulp-runtime').create();
+
+var watcher;
+watcher = gulp.watch('app/*.js', 'minify browserSync');
+
+gulp.watch('app/*.*', {
+  wait: true,  // run tasks after change in series
+  tasks: 'sass css jsx browserSync',
+  reload: true // will only reload app/*.js files (server, db, etc)
+}, function(ev, opt){
+  var reloaded = opt.reload && ev.type !== 'delete';
+  console.log('%s was %s', ev.path, ev.type, reloaded ? 'and reloaded' : '');
+});
+
+**/
+tornado.Runtime.prototype.watch = function(glob, opt, fn){
+  var cb = util.type(opt).function || util.type(fn).function;
+  var o = (!cb && util.type(fn).plainObject)
+    || (opt && util.type(opt).plainObject)
+    || {tasks: cb ? opt : null, wait: true};
+
+  var reload = o.reload && function reloadHandle(ev){
+    if(ev.type !== 'delete' && require.extension[path.extname(ev.path)]){
+      delete require.cache[ev.path];
+      require(ev.path);
+    }
+    if(cb){ cb(ev, o); }
+  };
+
+  var tasks = util.type(o && o.tasks || o);
+  if(tasks.string || tasks.array){
+    tasks = tasks.string || tasks.array.join(' ');
+    return vinylFS.watch(glob, null, reload
+      ? this.stack(tasks, reload, o)
+      : this.stack(tasks, o)
     );
   }
 
-  return vinylFS.watch(glob, opt, cb);
-};
-
-/*
-# runtime.stalk
-```js
-function stalk(string|array globs[, array opt, function callback])
-```
-Same as [runtime.watch][t-runtime-watch] but files are
- reloaded on the `require.cache` as they change. A callback is
-  provided if further processing is needed.
-
-_arguments_
- - same as [runtime.watch][t-runtime-watch] aside of `callback`
- - `callback` type function that will be called after file is reloaded
-
-_returns_
- - a watcher, same as [runtime.watch][t-runtime-watch]
-
-> Notes:
-- The arguments of the callback are the same as
- [runtime.watch][t-runtime-watch] (a gaze event)
-- When a file is deleted the reload will be skipped but the
-callback is still invoked
-
-*/
-tornado.Runtime.prototype.stalk = function(glob, opt, cb){
-  var self = this;
-  cb = typeof cb === 'function' && cb;
-  return this.watch(glob, opt, function(ev){
-    if(ev.type === 'delete'){
-      return cb ? cb(ev) : null;
-    }
-
-    delete require.cache[ev.path];
-    require(ev.path);
-
-    if(self.store.log){
-      console.log('Reloaded', util.color.file(ev.path));
-    }
-
-    if(cb){ cb(ev); }
+  return vinylFS.watch(glob, opt, reload || function onChange(ev){
+    if(cb){ cb(ev, o); }
   });
 };
 
@@ -158,11 +167,15 @@ tornado.Stack.prototype.onHandleNotFound = function(next){
 // Errors anywhere in the stack
 //
 tornado.Stack.prototype.onHandleError = function(error, next){
-  util.log(
-    '\'' + util.color.cyan(next.match) + '\'',
-    'has thrown an error after',
+  var file = error.stack.match(/\(?(\S+:\d+:\d+)\)?/);
+  file = path.resolve(process.cwd(), file[1] || file[2]);
+
+  util.log('Error in \'%s\' after %s',
+    util.color.cyan(next.match),
     util.color.time(next.time)
   );
+
+  util.log('  at  %s', util.color.file(file));
 
   if(!this.runtime.repl){ throw error; }
   this.log(error.stack);
@@ -182,15 +195,15 @@ tornado.Stack.prototype.onHandle = function(next){
   var host = this.host ? this.host.path : '';
   var mode = this.wait ? 'series' : 'parallel';
   var time, status = next.time ? 'Finished' : 'Wait for';
+  var indent = len && mode === 'parallel' ? '- ' : '';
 
   if(!this.time && len){
-    util.log('Started %s', '\'' + util.color.cyan(this.path) + '\'',
+    util.log('Started \'%s\'', util.color.cyan(this.path),
       host ? ('from ' + util.color.green(host)) : 'in',
       util.color.bold(mode)
     );
   } else if(next.time){
-    util.log((len ? '- ' : '') + status,
-      '\'' + util.color.cyan(path) + '\'' +
+    util.log('%s \'%s\' %s', indent + status, util.color.cyan(path),
       (time ? ' in ' + util.color.time(time) : '')
     );
   }
@@ -200,9 +213,9 @@ tornado.Stack.prototype.onHandle = function(next){
   if(this.queue || this.time.end){ return ; }
 
   this.time.end = util.color.time(this.time);
-  util.log('Finished', '\'' + util.color.cyan(this.path) + '\'',
-    (host ? 'from '+ util.color.green(host) + ' ' : '') +
-    'in ' + this.time.end
+  util.log('Finished \'%s\'%s in %s', util.color.cyan(this.path),
+    (host ? ' from '+ util.color.green(host) + ' ' : ''),
+    this.time.end
   );
 };
 
@@ -215,15 +228,36 @@ tornado.repl = function (name, o){
   if(app.store.children['--silent']){ return app; }
   // we have been here already ^
 
-  // --silent
+  // --no-color
   //
   app.set('--silent', function(next){
-
     if(process.argv.indexOf('--tasks-simple') < 0){
       app.store.log = !this.log;
     }
 
     console.log('logging %s', app.store.log ? 'enabled' : 'disabled');
+    next();
+  });
+
+  // --no-color, --color
+  //
+  app.set(':flag(--no-color|--color)', function(next){
+    var wrote = false;
+    if(util.color.enabled && this.params.flag === '--no-color'){
+      util.color.enabled = false;
+    } else if(!util.color.enabled && this.params.flag === '--color'){
+      util.color.enabled = true;
+    } else if(this.log){
+      util.log('color already %s',
+        util.color.enabled ? 'enabled' : 'disabled'
+      );
+      wrote = true;
+    }
+
+    if(!this.log || wrote){ return next(); }
+    util.log('color %s', util.color.enabled
+      ? util.color.bold('enabled') : 'disabled'
+    );
     next();
   });
 
@@ -272,19 +306,17 @@ tornado.repl = function (name, o){
         (isGulpfile ? 'gulpfile' : 'module') + ' ' +
         util.color.file(filename);
 
-      if(this.repl){
-        util.log(message); util.log(err.stack);
+      if(this.runtime.repl){
+        util.log(message);
+        util.log(err.stack);
         return next();
       } else {
-        throw new util.PluginError({
-          plugin: util.color.yellow('gulp-runtime'),
-          message: message
-        });
+        throw new Error(message);
       }
     }
 
     util.log(
-      (cached ? util.color.cyan('Reloaded') : 'Loaded'),
+      (cached ? 'Reloaded' : 'Loaded'),
       util.color.file(filename)
     );
 
