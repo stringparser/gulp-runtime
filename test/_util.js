cdb64c5167ce231621d03b4da933b574323efc86
Javier Carrillo <stringparser@gmail.com>
2015-04-14 12:46:07 +0200

    added through to utils, lint

--- test/_util.js
+++ test/_util.js
@@ -1,18 +1,16 @@
 'use strict';
 
-require('should');
 var fs = require('fs');
 
 module.exports = {
   suite : function(){
-    var first = [
-      'create.js'
-    ];
+    var first = ['create.js'];
 
     return first.concat(
       fs.readdirSync(__dirname).filter(function(file){
         return !/^_/.test(file) && first.indexOf(file) < 0;
       })
     );
-  }
+  },
+  through: require('through2')
 };
