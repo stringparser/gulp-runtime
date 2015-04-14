158c6a58620293744dfc728c2c723e23f666d561
Javier Carrillo <stringparser@gmail.com>
2015-04-14 12:53:21 +0200

    test for create done

--- test/create.js
+++ test/create.js
@@ -8,19 +8,35 @@ module.exports = function(runtime, util){
 	should.exists(util);
 	var create = runtime.create;
 
-	it('should be a tornado.Runtime instance by default', function(){
-		var gulp = create();
-		gulp.constructor.should.be.eql(tornado.Runtime);
+	it('should be a Runtime instance', function(){
+		var gulp = create('one');
+
+		gulp.should.be.instanceof(tornado.Runtime);
 	});
 
-	it('should be a readline.Interface create({repl: true})', function(){
-		var gulp = create({repl: true});
+	it('should have no repl by default', function(){
+		var gulp = create('two');
+
+		gulp.should.not.have.property('repl');
+	});
+
+	it('should be readline.Interface for {repl: true}', function(){
+		var gulp = create('three', {repl: true});
 
 		gulp.repl.should.be.instanceof(readline.Interface);
+
 		gulp.repl.close();
 	});
 
-	it('should retrieve a previous instance if exists', function(){
+	it('should be readline.Interface for {input: true}', function(){
+		var gulp = create({input: true});
+
+		gulp.repl.should.be.instanceof(readline.Interface);
+
+		gulp.repl.close();
+	});
+
+	it('should give a previous instance if it exists', function(){
 		var build = create('build');
 
 		create('build').should.be.eql(build);
