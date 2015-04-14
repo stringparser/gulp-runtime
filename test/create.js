62769ab374ce26fa330259afa35dc40e448d510d
Javier Carrillo <stringparser@gmail.com>
2015-04-14 12:55:36 +0200

    added repl assertion

--- test/create.js
+++ test/create.js
@@ -10,35 +10,32 @@ module.exports = function(runtime, util){
 
 	it('should be a Runtime instance', function(){
 		var gulp = create('one');
-
 		gulp.should.be.instanceof(tornado.Runtime);
 	});
 
 	it('should have no repl by default', function(){
 		var gulp = create('two');
-
 		gulp.should.not.have.property('repl');
 	});
 
 	it('should be readline.Interface for {repl: true}', function(){
 		var gulp = create('three', {repl: true});
-
+		gulp.should.have.property('repl');
 		gulp.repl.should.be.instanceof(readline.Interface);
-
 		gulp.repl.close();
 	});
 
 	it('should be readline.Interface for {input: true}', function(){
 		var gulp = create({input: true});
-
+		gulp.should.have.property('repl');
 		gulp.repl.should.be.instanceof(readline.Interface);
-
 		gulp.repl.close();
 	});
 
 	it('should give a previous instance if it exists', function(){
 		var build = create('build');
-
+		
+		build.should.not.have.property('repl');
 		create('build').should.be.eql(build);
 		create('build', {repl: true}).should.be.eql(build);
 		create('build', {input: process.stdin}).should.be.eql(build);
