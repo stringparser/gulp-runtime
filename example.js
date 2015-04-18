e03ccedf7ddf6d3740867020649de428520138ab
Javier Carrillo <stringparser@gmail.com>
2015-04-18 20:10:10 +0200

    smoke tests for watch

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ gulp.task(':handle(css|jsx|img)', function(next){
  next();
 });
 
-gulp.stack('jsx css img', {wait: true})();
+gulp.stack('jsx css img')();
 
 vfs.watch('test/*.js', null, function(){
   console.log(arguments);
