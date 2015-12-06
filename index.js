2e6a42702e8ad5fecc13a05155c98e4119d3c986
Javier Carrillo <stringparser@gmail.com>
2015-12-06 14:14:11 +0100

    dev: prepare to use new verision of runtime

--- index.js
+++ index.js
@@ -1,151 +1,124 @@
 'use strict';
 
-var path = require('path');
-var util = require('./util');
-var tornado = require('./lib/proto');
+var util = require('./lib/util');
 
-/*
-## documentation
+var Parth = require('parth');
+var Runtime = require('runtime');
+var vinylFS = require('vinyl-fs');
 
-#### module.exports
+var Gulp = module.exports = Runtime.createClass({
+  src: vinylFS.src,
+  dest: vinylFS.dest,
+  create: function(props, Super){
+    props = props || {};
+    Super.apply(this, props);
+    this.tasks = new Parth();
+    this.watchers = {};
 
-The `module.exports` a function
-
-```js
-var create = require('gulp-runtime');
-```
-
-#### create
-```js
-function create([string name|object options, object options])
-```
-A key value instance store. When there is no instance `name`
-a new one is retrieved, if it already exists that is returned istead.
-
-_arguments_
- - `name` type string, the name given for the instance
- - `options` type object, options passed down to the instance
-  - `options.log` type boolean, whether to log or not
-  - `options.repl` type boolean, whether to make a repl with the instance
-  - `options.input` type stream, input stream for the repl
-  - `options.output` type stream, output stream for the repl
-
-_defaults_
- - `name` defaults to `#root`
- - if `options.repl` or `options.input` is truthy a repl is created
-  at `instance.repl` using the [readline][m-readline] module
-  - if `options.input` is not a stream, to `process.stdin`
-  - if `options.output` is not a stream, to `process.stdout`
-
-_returns_
- - an existing instance `name` if `name` is given
- - a new instance if there wasn't `name` wasn't created before
-
-*/
-function create(name, o){
-  o = util.type(o || name).plainObject || {};
-  name = util.type(name).string || '#root';
-
-  var app = o.repl || o.input
-    ? tornado.repl(name, o)
-    : tornado.create(name, o);
-
-  // default commands were alredy attached
-  //
-  if(app.store.children[':flag(--silent)']){ return app; }
-
-  // --no-color
-  //
-  app.set(':flag(--silent)', function(next){
-    if(this.path.indexOf('--tasks-simple') < 0){
-      this.log = app.store.log = !this.log;
+    if(this.repl === true){
+      util.createREPL(this);
     }
-    next();
+  }
+});
+
+/**
+ * gulp.task
+ **/
+Gulp.prototype.task = function(name, tasks, fn){
+  fn = util.type(fn || tasks).function || '';
+  name = util.type(name || fn.displayName || fn.name).string;
+  tasks = util.type(tasks).array || util.type(name).array;
+
+  if(name && tasks && fn){
+    var handle = this.stack.apply(this, tasks.concat(fn, {wait: true}));
+    this.tasks.set(name, {handle: handle});
+  } else if(name && fn){
+    fn.displayName = name;
+    this.tasks.set(name, {handle: fn});
+  } else if(name){
+    return this.tasks.get(name);
+  } else if(!name){
+    throw new TypeError('cannot get a task without a name');
+  } else if(!fn){
+    throw new TypeError('cannot set a task without a function');
+  }
+
+  return this;
+};
+
+/**
+ * gulp.dest
+ **/
+Gulp.prototype.watch = function(glob, opt, fn) {
+  var self = this, watcher;
+
+  if (typeof opt === 'function' || Array.isArray(opt)) {
+    fn = opt;
+    opt = null;
+  }
+
+  // Array of tasks given
+  if (Array.isArray(fn)) {
+    watcher = vinylFS.watch(glob, opt, function() {
+      self.stack.apply(self, fn)();
+    });
+  } else {
+    watcher = vinylFS.watch(glob, opt, fn);
+  }
+
+  this.watchers[glob] = watcher;
+  watcher.once('end', function(){
+    self.watchers[glob] = null;
   });
 
-  // --cwd
-  //
-  app.set(':flag(--cwd) :dirname', function(next){
-    var cwd = process.cwd();
-    var dirname = path.resolve(cwd, next.params.dirname);
-    if(dirname !== cwd){ process.chdir(dirname); }
-    if(this.log){
-      util.log('Working directory changed to',
-        util.color.file(dirname)
-      );
-    }
-    next();
-  });
+  return watcher;
+};
 
-  // --no-color, --color
-  //
-  app.set(':flag(--no-color|--color)', function(next){
-    util.color.enabled = this.params.flag === '--color';
-    if(!this.log){ return next(); }
-    util.log('color %s',util.color.enabled
-      ? util.color.bold('enabled')
-      : 'disabled'
+Gulp.prototype.reduceStack = function(stack, site){
+  var task = typeof site !== 'function'
+    ? this.tasks.get(site)
+    : {path: site.displayName || site.name, handle: site};
+
+  if(!task || typeof task.handle !== 'function'){
+    throw new Error('no task handle defined for `' + site + '`');
+  } else if(this.log){
+    if(!stack.label){ stack.label = []; }
+    stack.push(task.handle);
+    stack.label.push(task);
+  } else {
+    stack.push(task.handle);
+  }
+
+  return stack;
+};
+
+Gulp.prototype.onHandle = function(next, task, stack){
+  if(!this.log || /^-/.test(task.path)){
+    return; // ^ skip logging for errors, silent or flags
+  } else if(stack.host && !stack.host.path){
+    var host = stack.host.path = stack.host.tree().label;
+  } else {
+    host = stack.host && stack.host.path || '';
+  }
+
+  if(!stack.time){
+    stack.path = stack.tree().label;
+    util.log('%sStarted \'%s\' in %s',
+      host, util.color.cyan(stack.path)
     );
-    next();
-  });
+    stack.time = process.hrtime();
+  } else if(next.time && task.path !== stack.path){
+    util.log('%sFinished \'%s\' after %s',
+      host,
+      util.color.cyan(task.path),
+      util.color.time(next.time)
+    );
+  }
 
-  // --tasks, -T or --tasks-simple
-  //
-  app.set(':flag(--tasks-simple|--tasks|-T)', function (next){
-    if(next.params.flag === '--tasks-simple'){
-      util.logTasksSimple(this.runtime);
-    } else {
-      util.logTasks(this.runtime);
-    }
-    next();
-  });
+  if(!next.time){ next.time = process.hrtime(); }
 
-  /*
-   --require, --gulpfile is the same but changing the cwd
-   */
-  app.set(':flag(--require|--gulpfile) :file', function (next){
-    var cwd = process.cwd();
-    var file = path.resolve(cwd, next.params.file);
-    var cached = Boolean(require.cache[file]);
-    var isGulpfile = /--gulpfile/.test(this.queue);
-    if(cached){ delete require.cache[file]; }
-
-    try {
-      require(file);
-    } catch(err){
-      util.log('Could not load %s %s', (isGulpfile ? 'gulpfile' : 'module'),
-        util.color.file(file)
-      );
-
-      if(this.runtime.repl){
-        util.color.callersPath(err.stack);
-        console.log(err.stack);
-        return next();
-      } else { throw err; }
-    }
-
-    if(this.log){
-      util.log((cached ? 'Reloaded' : 'Loaded'), util.color.file(file));
-    }
-
-    var dirname = path.dirname(file);
-    if(isGulpfile && dirname !== cwd){
-      process.chdir(dirname);
-      if(this.log){
-        util.log('Working directory changed to',
-          util.color.file(dirname)
-        );
-      }
-
-      // update "gulpfile"
-      app.set({configFile: file});
-    }
-    next();
-  });
-
-  return app;
-}
-
-exports = module.exports = {
-  create: create
+  stack.time.end = util.color.time(stack.time);
+  util.log('%sFinished \'%s\' after %s',
+    host, util.color.cyan(stack.path), stack.time.end
+  );
 };
