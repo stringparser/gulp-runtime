975eb01e0ee976123a3c7a106bd6163b37ac948e
Javier Carrillo <stringparser@gmail.com>
2016-01-24 20:37:10 +0100

    tests: making new tests

--- test/create.js
+++ test/create.js
@@ -1,45 +1,30 @@
 'use strict';
 
 var should = require('should');
-var tornado = require('tornado-repl');
 var readline = require('readline');
 
-module.exports = function(runtime, util){
-	should.exists(util);
-	var create = runtime.create;
+module.exports = function(Gulp, util){
+  should.exists(util);
 
-	it('should be a Runtime instance', function(){
-		var gulp = create('one');
-		gulp.should.be.instanceof(tornado.Runtime);
-	});
+  it('create() should log by default', function(){
+    var gulp = Gulp.create();
+    gulp.log.should.be.eql(true);
+  });
 
-	it('should have no repl by default', function(){
-		var gulp = create('two');
-		gulp.should.not.have.property('repl');
-	});
+  it('create() should have no repl by default', function(){
+    var gulp = Gulp.create();
+    gulp.should.not.have.property('repl');
+  });
 
-	it('should be readline.Interface for {repl: true}', function(){
-		var gulp = create('three', {repl: true});
-		gulp.should.have.property('repl');
-		gulp.repl.should.be.instanceof(readline.Interface);
-		gulp.repl.close();
-	});
+  it('create({log: false}) should disable logging', function(){
+    var gulp = Gulp.create({log: false});
+    gulp.log.should.be.eql(false);
+  });
 
-	it('should be readline.Interface for {input: true}', function(){
-		var gulp = create({input: true});
-		gulp.should.have.property('repl');
-		gulp.repl.should.be.instanceof(readline.Interface);
-		gulp.repl.close();
-	});
-
-	it('should give a previous instance if it exists', function(){
-		var build = create('build');
-		
-		build.should.not.have.property('repl');
-		create('build').should.be.eql(build);
-		create('build', {repl: true}).should.be.eql(build);
-		create('build', {input: process.stdin}).should.be.eql(build);
-
-		build.repl.close();
-	});
+  it('create({repl: true}) should make a repl at gulp.repl', function(){
+    var gulp = Gulp.create({repl: true});
+    gulp.should.have.property('repl');
+    gulp.repl.should.be.instanceof(readline.Interface);
+    gulp.repl.close();
+  });
 };
--- test/task.js
+++ test/task.js
@@ -6,65 +6,40 @@ module.exports = function(runtime, util){
   should.exists(util);
   var create = runtime.create;
 
-  it('should throw if handle does not use a callback or return', function(){
-    var gulp = create('throw');
-
-    (function(){
-      gulp.task('name', function(){});
-    }).should.throw();
-  });
-
-  it('should register task deps as an array', function(){
-    var gulp = create('array deps');
-    var dep = ['1', '2'];
-
-    gulp.task('name', dep);
-    gulp.get('name')
-      .should.have.property('dep', dep);
-  });
-
-  it('should register task deps as a string', function(){
-    var gulp = create('string deps');
-    var dep = ' 1  2 3';
-
-    gulp.task('name', dep);
-    gulp.get('name')
-      .should.have.property('dep', dep.trim().split(/[ ]+/));
-  });
-
-  it('should register task handle', function(){
-    var handle = function(next){ next(); };
-    var gulp = create('new task');
-
+  it('task("name", [Function]) should register `name`', function(){
+    var gulp = create();
+    var handle = function(){};
     gulp.task('name', handle);
-    gulp.get('name').should.have.property('handle', handle);
+    gulp.tasks.get('name').fn.should.be.eql(handle);
   });
 
-  it('deps should run in series and before task', function(done){
-    var stack = [];
-    var dep = 'one two';
-    var gulp = create('run deps');
-    var randomHandle = function(next){
-      setTimeout(next, Math.random()*10);
-    };
+  it('task([Function: name]) should register `name`', function(){
+    var gulp = create();
+    function name(){}
+    gulp.task(name);
+    gulp.tasks.get('name').fn.should.be.eql(name);
+  });
 
-    gulp.task('three', dep, randomHandle);
-    gulp.task('one', randomHandle);
-    gulp.task('two', randomHandle);
+  it('task("taskName", [Function: name]) should register `name`', function(){
+    var gulp = create();
+    function name(){}
+    gulp.task('taskName', name);
+    gulp.tasks.get('taskName').fn.should.be.eql(name);
+  });
 
-    gulp.set({
-      log: false,
-      onHandleEnd: function(next){
-        if(next.match){ stack.push(next.match); }
-        if(this.queue){ return ; }
-        if(this.host){
-          this.wait.should.be.eql(true);
-          stack.should.be.eql(['one', 'two', 'three']);
-          done();
-        }
-      }
-    });
+  it('task("taskName") should return registered its function', function(){
+    var gulp = create();
+    function name(){}
+    gulp.task('taskName', name);
+    gulp.task('taskName').should.be.eql(name);
+  });
 
-    gulp.stack('three')();
+  it('task("taskName", deps, [Function]) should register task deps', function(){
+    var gulp = create();
+    var deps = ['1', '2', '3'];
+    function name(){}
+
+    gulp.task('taskName', deps, name);
+    gulp.task('taskName').should.be.eql(name);
   });
 };
