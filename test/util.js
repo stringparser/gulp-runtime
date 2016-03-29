36ea334a98a243bbbc576833a40e7c6c43470666
Javier Carrillo <stringparser@gmail.com>
2016-03-29 18:18:30 +0200

    tests: move testFile to test/util

--- test/util.js
+++ test/util.js
@@ -6,6 +6,7 @@ module.exports = {
   rimraf: require('rimraf'),
   through: require('through2'),
   content: 'exports = module.exports = {}',
+  testFile: 'test/dir/watch.js',
   testModule: 'test/dir/module.js',
   contentChanged: 'exports = module.exports = {content: \'changed\'}'
 };
