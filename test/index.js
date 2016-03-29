019e2ed7a1b5a1e814811a71612c9f0338706028
Javier Carrillo <stringparser@gmail.com>
2016-03-29 18:17:46 +0200

    tests: include watch in tests, disable loggin for testing

--- test/index.js
+++ test/index.js
@@ -3,18 +3,23 @@
 var path = require('path');
 
 var Gulp = require('../');
-var util = require('./util');
+var util = require('../lib/util');
+var testUtil = require('./util');
 
 var tests = [
   'create.js',
-  'task.js'
+  'task.js',
+  'watch.js'
 ];
+
+// disable logging for tests
+util.log = function(){}
 
 describe(require('../package').name, function(){
   tests.forEach(function(file){
     var suite = path.basename(file, path.extname(file));
     describe(suite, function(){
-      require('./' + file)(Gulp, util);
+      require('./' + file)(Gulp, testUtil);
     });
   });
 });
