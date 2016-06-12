386a040f5e0d1d78160805f3f5f329b23f5a16d0
Javier Carrillo <stringparser@gmail.com>
2016-06-12 13:38:40 +0200

    test: simplify and improve cli tests

--- test/cli.js
+++ test/cli.js
@@ -4,48 +4,32 @@ var fs = require('fs');
 var path = require('path');
 var should = require('should');
 
-module.exports = function(Gulp, util){
-  should.exists(util);
+var test = {
+  file: path.resolve(__dirname, 'dir', 'cliTestFile.js'),
+  dirname: path.resolve(__dirname, 'dir'),
+  content: 'exports = module.exports = { test: "cli" };'
+};
+
+exports = module.exports = function(Gulp, util){
 
   before(function(done){
-    util.mkdirp('test/dir', function(e){
-      if(e){ done(e); }
-      fs.writeFile(util.testModule, util.testContent, function(er){
-        if(er){ done(er); }
-        fs.writeFile('test/dir/gulpfile.js', '', function(err){
-          if(err){ done(err); }
-          done();
-        });
-      });
+    util.mkdirp(test.dirname, function(error){
+      if(error){ return done(error); }
+      fs.writeFile(test.file, test.content, done);
     });
   });
 
   after(function(done){
-    util.rimraf('test/dir', done);
+    util.rimraf(test.dirname, done);
   });
-
-  var cliCommands = [
-    ':cli(--silent)',
-    ':cli(--version|-v)',
-    ':cli(--cwd) :dirname(\\S+)',
-    ':cli(--no-color|--color)',
-    ':cli(--tasks-simple|--tasks|-T)',
-    ':cli(--require) :file(\\S+)'
-  ];
 
   var gulp = Gulp.create();
-
-  it('should have all CLI commands', function(){
-    cliCommands.forEach(function(name){
-      (gulp.tasks.get(name) !== null).should.be.eql(true);
-    });
-  });
 
   it('--silent should turn off logging', function(done){
     gulp.start('--silent', {
       onHandleError: done,
       onHandleEnd: function(){
-        this.log.should.be.eql(false);
+        should(this.log).be.eql(false);
         done();
         gulp.log = true;
       }
@@ -56,7 +40,7 @@ module.exports = function(Gulp, util){
     gulp.start('--no-color', {
       onHandleError: done,
       onHandleEnd: function(){
-        util.lib.chalk.should.have.property('enabled', false);
+        should(util.lib.chalk).have.property('enabled', false);
         done();
       }
     });
@@ -66,7 +50,7 @@ module.exports = function(Gulp, util){
     gulp.start('--color', {
       onHandleError: done,
       onHandleEnd: function(){
-        util.lib.chalk.should.have.property('enabled', true);
+        should(util.lib.chalk).have.property('enabled', true);
         done();
       }
     });
@@ -75,35 +59,26 @@ module.exports = function(Gulp, util){
   it('--cwd :dirname should change cwd', function(done){
     var cwd = process.cwd();
 
-    gulp.stack('--cwd ./dir/module', {
+    gulp.start('--cwd ' + test.dirname, {
       onHandleError: done,
-      onHandleStart: function(){
-        console.log('helloo');
-      },
       onHandleEnd: function(){
-        console.log('hellooo');
-        console.log(process.cwd());
-        process.cwd().should.be.eql(
-          path.resolve(__dirname, 'dir')
-        );
+        should(process.cwd()).be.eql(test.dirname);
+        process.chdir(cwd); // restore previous working directory
+        done();
       }
-    })(function (){
-      process.chdir(cwd); // restore previous working directory
-      done();
     });
   });
 
   it('--require should require a module from the cwd', function(done){
     var cwd = process.cwd();
-    var modulePath = path.join(cwd, 'test/dir/module.js');
 
-    gulp.start('--require test/dir/module', {
+    gulp.start('--require ' + test.file, {
       onHandleError: done,
       onHandleStart: function(){
-        require.cache.should.not.have.property(modulePath);
+        should(require.cache).not.have.property(test.file);
       },
       onHandleEnd: function(){
-        require.cache.should.have.property(modulePath);
+        should(require.cache).have.property(test.file);
         process.cwd().should.be.eql(cwd);
         done();
       }
