cce01942801e5790a3e4047af06083d3ae80cef2
Javier Carrillo <stringparser@gmail.com>
2016-06-12 13:40:38 +0200

    test: cleanup test/util.js

--- test/util.js
+++ test/util.js
@@ -4,9 +4,5 @@ module.exports = {
   lib: require('../lib/util'),
   mkdirp: require('mkdirp'),
   rimraf: require('rimraf'),
-  through: require('through2'),
-  content: 'exports = module.exports = {}',
-  testFile: 'test/dir/watch.js',
-  testModule: 'test/dir/module.js',
-  contentChanged: 'exports = module.exports = {content: \'changed\'}'
+  through: require('through2')
 };
